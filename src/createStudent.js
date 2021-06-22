816ba49c974fec55ca8addc9afe157c382bb713e
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-06-22 15:50:27 -0300

    Challenge 5 completed

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (string) => {
+  if (!string) return undefined;
+  return {
+    name: string,
+    feedback() {
+      return 'Eita pessoa boa!';
+    },
+  };
+};
 
 module.exports = createStudent;
