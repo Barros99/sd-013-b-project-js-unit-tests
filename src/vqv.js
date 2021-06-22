cfdf407b8f21fd342eedbf37ba7760cca220a55d
alanmdf <alanmoreirascr@gmail.com>
2021-06-22 15:35:12 -0300

    Requisito 3 ok no teste ok no linter

--- src/vqv.js
+++ src/vqv.js
@@ -19,7 +19,10 @@
 const vqv = (nome, idade) => { 
   if (typeof (nome) !== 'string' || typeof (idade) !== 'number') {
     return undefined;
-  } return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  } return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
