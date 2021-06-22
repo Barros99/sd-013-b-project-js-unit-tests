13daa5b8a23b8822189e1453ef888cb650ea1f99
Julio Cesar <juliosinu@gmail.com>
2021-06-22 16:44:05 -0300

    primeiro requisito completo

--- src/average.js
+++ src/average.js
@@ -12,6 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let contador = 0;
+  if(array.length === 0){
+    return undefined;
+  }
+
+  for(let index = 0; index < array.length; index += 1){
+    if(typeof(array[index]) !== 'number') {
+      return undefined;
+    } else
+      contador += array[index];
+    }  
+  return Math.round(contador/array.length);
+};
+
+console.log(average([]));
+
 
 module.exports = average;
