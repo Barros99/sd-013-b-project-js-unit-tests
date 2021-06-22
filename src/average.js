5d530b80b6fee59c195a53f12d8e615c3da35f0b
eaeniko <eaenikolai@gmail.com>
2021-06-22 16:17:25 -0300

    [Nikolai] requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let sum = 0;
+  let result = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      result = undefined;
+      console.log(typeof(array[index]));
+      return result;
+    } else {
+      sum += Math.round(array[index]);
+      result = Math.round(sum / array.length);
+    }
+  }
+  return result;
 };
 
 module.exports = average;
