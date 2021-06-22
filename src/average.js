f0cb41a1d746461efbe3f702719a63954d9547db
GenesisHenriques <genesishenriques7@gmail.com>
2021-06-22 16:02:45 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -13,6 +13,22 @@
     
 */
 
-const average = () => {};
+const average = (array) => {
+  let total = 0;
+
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    total += array[index];
+  }
+  return Math.round(total / array.length);
+};
+
+
 
 module.exports = average;
