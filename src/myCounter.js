bd0800b021aaa49ee397b7cdab44567389a2df6f
Joao Victor <joaogmavictor@gmail.com>
2021-06-22 16:43:41 -0300

    Concluindo requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,16 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let indice = 2; indice <= 3; indice += 1) {
+      myArray.push(indice);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
--- src/objCalculator.js
+++ src/objCalculator.js
@@ -26,5 +26,4 @@ const calculator = {
   sub: (num1, num2) => num1 - num2,
 };
 
-console.log(calculator.div(5, 2));
 module.exports = calculator;
