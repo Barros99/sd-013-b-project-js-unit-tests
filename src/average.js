ba0cdcaabff4cc71a994b2c9eab69fff46c52d47
Othyose <arth_nf@hotmail.com>
2021-06-22 16:10:04 -0300

    Commit inicial. VQV!!

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  let media = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+    media = sum / array.length;
+  }
+  const result = media.toFixed(0);
+  return Number(result);
+};
 
 module.exports = average;
