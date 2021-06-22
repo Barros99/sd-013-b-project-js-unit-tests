982002c3419e3b6446955757203f0e6f65a4bc4b
Victor Mendonca <victor.men.dev@gmail.com>
2021-06-22 16:02:15 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let soma = 0;
+  let media = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) === 'number') {
+      soma += array[index];
+    } else {
+      return undefined;
+    }
+  }
+  media = soma / array.length;
+  return Math.round(media);
 };
 
 module.exports = average;
