66017c15a5bf8edf8cdbf5c299e1d0c128438c95
Rafael Friederick <rafael.mf.documents@gmail.com>
2021-06-22 15:07:56 -0300

    Requisito 1 finalizado

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let Sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    Sum += array[index];
+  }
+  return Math.round(Sum / array.length);
+};
 
 module.exports = average;
