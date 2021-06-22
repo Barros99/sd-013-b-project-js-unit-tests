fd36e1fc108b57cb9bd5c2fbab9189449a261279
alanmdf <alanmoreirascr@gmail.com>
2021-06-22 16:07:58 -0300

    Requisito 6 concluido

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,20 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
+    // Após pesquisa, descobri o método Array.isArray(). Link de referência: https://pt.stackoverflow.com/questions/77190/como-saber-se-um-objeto-%C3%A9-um-array-em-javascript-sem-jquery
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(productDetails()[0]), 'object');
+    assert.strictEqual(typeof(productDetails()[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails()[0], productDetails()[1]);
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.endsWith('123'), true);
+    assert.strictEqual(productDetails()[1].details.productId.endsWith('123'), true);
+    // Após pesquisa, descobri o método endsWith(). Link de referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
   });
 });
