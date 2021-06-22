8f4d685ad53fa3dc6021b179ac78501888cbc00f
Joao Victor <joaogmavictor@gmail.com>
2021-06-22 15:54:58 -0300

    Concluindo requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -14,7 +14,7 @@ const circle = require('../src/circle');
   Comportamento:
     - circle(1) // Retorno: {radius: 1, area: 3.14, circumference: 6.28}
     - circle(7) // Retorno: {radius: 7, area: 153.86, circumference: 43.96}
-    - circle(3) // Retorno: {radius: 3, area: 28,26, circumference: 18.84}
+cccc
 
   DICA: Números de ponto flutuante em JavaScript são imprecisos!  Para testar, vá no seu navegador e faça `0.2 + 0.1`.
         Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
@@ -25,13 +25,25 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.deepStrictEqual(circle(1), {radius: 1, area: 3.14, circumference: 6.28})
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.entries(circle(1)).length,3)
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined)
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2).circumference, 12.56)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(parseInt(parseFloat(circle(3).area).toPrecision(2)), 28)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const area = parseFloat((circle(3).area).toPrecision(2))
+    const circumference = parseFloat((circle(3).circumference).toPrecision(2))
+    const radius = parseFloat((circle(3).radius).toPrecision(2))
+    assert.strictEqual(area, 28)
+    assert.strictEqual(circumference, 19)
+    assert.strictEqual(radius, 3)
+  
+    // { radius: 3, area: 28.26, circumference: 18.84 }
   });
 });
