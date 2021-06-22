312db9cc4896ea58b76aa2b1d75aeeafad21d69b
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-06-22 16:53:49 -0300

    desafio 1 - falta resolver a complexidade do lint

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (array) => {
+  let somatoria = 0;
+  if (array.length > 0) {
+    for (let index = 0; index < array.length; index += 1) {
+      if (typeof (array[index]) === 'number') {
+        somatoria += array[index];
+      } else {
+        return undefined;
+      }
+    }
+  } else {
+    return undefined;
+  }
+  let media = somatoria / array.length;
+  return Math.round(media);
+};
 module.exports = average;
