657dfea84d2b556b8dc3f2c759697d64014cd5ed
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-06-22 15:51:20 -0400

    requisito create student

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let dados = {
+    name: nome,
+    feedback: () => `Eita pessoa boa!`,
+  };
+  return dados;
+};
 
 module.exports = createStudent;
