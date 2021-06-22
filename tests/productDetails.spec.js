f701a9182c02b78e8ab3fbdb3113af396b7f655c
k4nl <gustavoh.bh@gmail.com>
2021-06-22 20:59:25 -0300

    refatorando desafio 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,8 +31,7 @@ const productDetails = require('../src/productDetails');
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     const resultProduct = productDetails('xablau', 'xableu');
-    const productId0 = resultProduct[0].details.productId.substr(-3)
-    const productId1 = resultProduct[1].details.productId.substr(-3);
+    const productId0 = [resultProduct[0].details.productId.substr(-3), resultProduct[1].details.productId.substr(-3)];
 
     // Teste que o retorno da função é um array.
     assert.strictEqual(typeof resultProduct, 'object')
@@ -43,7 +42,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(Object.values(resultProduct[0]), Object.values(resultProduct[1]));
     // Teste que os dois productIds terminam com 123.
-    assert.deepStrictEqual(productId0, productId1);  // .substr(-3);
+    assert.deepStrictEqual(productId0, ['123', '123']);  // .substr(-3);
   });
 });
 
