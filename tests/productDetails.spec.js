7e16edd4c322aaf0ddd00b2c614508a470663157
Pedro Brito <pedroab0119@gmail.com>
2021-06-22 19:45:56 -0300

    Req6 done

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,15 +27,21 @@ const productDetails = require('../src/productDetails');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
+const productDetailsReturn = productDetails('Alcool gel', 'Máscara');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray((productDetails())), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof(productDetails()), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productDetailsReturn[0] !== productDetailsReturn[1], true);
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetailsReturn[0].details.productId.slice(-3), '123');
+    assert.strictEqual(productDetailsReturn[1].details.productId.slice(-3), '123');
   });
 });
