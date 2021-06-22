d69c75001cb1bdb826fc2e53bdba4427315185a0
José Breno <brenojbs@hotmail.com>
2021-06-22 18:16:15 -0300

    Refatorando por causa do lint novamente

--- src/average.js
+++ src/average.js
@@ -20,7 +20,8 @@ const average = (array) => {
   for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') {
       return undefined;
-    } else if (array[index] === 0) {
+    } 
+    if (array[index] === 0) {
       return 0;
     }
     sum += array[index];
