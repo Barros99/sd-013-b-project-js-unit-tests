d8ed0e7e30f8bcf7bce33187cc95f02b56bf42cb
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-06-22 16:26:10 -0300

    Adicionando exercicio 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const feedback = () => ('Eita pessoa boa!');
+
+const createStudent = (nome) => ({ name: nome, feedback });
+
+const estudante = createStudent('Leandrão, o Lobo Solitário');
 
 module.exports = createStudent;
