7d015b997b083337d4e692a89866179a0ac4a87d
gustavoalves23 <gustavo.alves388@gmail.com>
2021-06-22 15:00:58 -0300

    Primeiro desafio

--- src/average.js
+++ src/average.js
@@ -12,8 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+const average = (numbers) => {
   // add your implementation here
+let sum = 0;
+if (numbers.length <= 0) {
+  return undefined;
+}
+for (let number = 0; number < numbers.length; number += 1) {
+  if (typeof (numbers[number]) !== 'number') {
+    return undefined;
+  }
+  sum += numbers[number];
+}
+
+return (Math.round(sum / numbers.length));
 };
 
 module.exports = average;
