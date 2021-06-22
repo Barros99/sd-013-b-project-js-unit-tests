d287ca0c082737396d9646df76c9810da1d83099
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-06-22 14:48:45 -0300

    resolv o primeiro requisito

--- src/average.js
+++ src/average.js
@@ -12,6 +12,13 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+  }
+  let averageValue = sum / array.length;
+  return averageValue;
+};
 
 module.exports = average;
