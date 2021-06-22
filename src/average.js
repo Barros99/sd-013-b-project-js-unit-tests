325b8437277ea2adf5432dbe819005a8fcdd21ec
k4nl <gustavoh.bh@gmail.com>
2021-06-22 15:11:15 -0300

    exercicio 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+let count = 0;
+if (array.length === 0) { 
+  return undefined;
+}
+for (let i = 0; i < array.length; i += 1) {
+  if (typeof array[i] === 'number') {
+    count += array[i];
+    } else {
+      return undefined;
+    }
+  }
+  return Math.round(count / (array.length));
+};
 
 module.exports = average;
