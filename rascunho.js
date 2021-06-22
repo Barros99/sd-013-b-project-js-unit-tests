46909633850d2bdd4da69546b407f3f8aacca1e7
Guilherme da Costa <guilhermedc93@gmail.com>
2021-06-22 16:38:39 -0300

    conclui etapa 3

--- rascunho.js
+++ rascunho.js
@@ -1,12 +1,1 @@
-const assert = require('assert')
-
-const numbers = (myArray) => {
-    for (let i = 0; i < myArray.length; i += 1) {
-      if (typeof myArray[i] !== 'number') {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
+console.log('Oi, meu nome é Alberto!\n' + 'Tenho 30 anos,\n')
