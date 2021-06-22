279c96d33854cc4f2ce4fd6899401a277607562d
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-22 18:16:36 -0300

    resolve a questao 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {  
+  const createPerson = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return createPerson;
+};
 
 module.exports = createStudent;
