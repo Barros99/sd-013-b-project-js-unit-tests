38a4d01ec42f0c5f7fcee3d02f60fce3266a5f8a
thiagoteixas <thiagoteixas@gmail.com>
2021-06-22 16:09:40 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let resposta;
+  if (typeof nome !== 'undefined' && typeof idade !== 'undefined') {
+    resposta = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  }
+  
+  return resposta;
+};
 
 module.exports = vqv;
