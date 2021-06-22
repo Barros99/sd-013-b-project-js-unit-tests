a6680543f9cc4b9780d969d97b1310d397017d91
Leonardo Martins <leonardomartins07@gmail.com>
2021-06-22 15:49:45 -0300

    part 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  let div = array.length;
+
+  if (array == 0) { return undefined; }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') { return undefined; }
+    sum += array[index];
+  }
+
+  return Math.round(sum / div);
+};
 
 module.exports = average;
