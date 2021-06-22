47cdbba5ea1ef83ff5517bb585999482395d4fe7
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-22 16:40:05 -0300

    desafio 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const goodPerson = 'Eita pessoa boa!';
+
+const feedback = () => 'Eita pessoa boa!';
+
+const createStudent = (nome) => ({ name: nome, feedback });
 
 module.exports = createStudent;
