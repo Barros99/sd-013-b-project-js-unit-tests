9369a97edd63dd71a9fdc86e2f7f0aa8f03924b6
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-06-22 19:54:42 -0300

    Exercise 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { access } = require('fs');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -30,12 +31,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof(productDetails('Alcool gel', 'Máscara')), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    const objectOne = Object.values(productDetails('Alcool gel', 'Máscara')[0])
+    const objectTwo =Object.values(productDetails('Alcool gel', 'Máscara')[1])
+    assert.deepStrictEqual(objectOne !== objectTwo, true)
     // Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(objectOne[1].productId.slice(-3) === objectTwo[1].productId.slice(-3), true);
   });
 });
