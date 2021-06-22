3833709e4b3bd386b191a111cce4b22fdfc21f9f
Alef Sloan <Alef_sloan@hotmail.com>
2021-06-22 18:16:35 -0300

    Requisito 5 finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (str) => {
+  let estudante = {
+    name: str,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 module.exports = createStudent;
