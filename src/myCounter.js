207c31392f757ceb700f6123787b1f2727ad517e
Jonmorais <jonathan_ms15@hotmail.com>
2021-06-23 10:27:31 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,12 +15,12 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
+let myArray = [];
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let secondCounter = 2; secondCounter <= 3; secondCounter += 1) {
+      myArray.push(secondCounter);
     }
   }
   return myArray;
