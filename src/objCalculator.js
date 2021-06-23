3c461be0cf7120069e4cb0956ea0fa83bd9d5f37
Barros99 <jeanmeneses@hotmail.com>
2021-06-23 12:21:46 -0300

    requisito 7 finalizado

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -17,8 +17,14 @@
   Comportamento:
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
+  Utilizar o Math.floor para arredondar para baixo !!!
 */
 
-const calculator = {};
+const calculator = {
+  add: (number1, number2) => Math.floor(number1 + number2),
+  mult: (number1, number2) => Math.floor(number1 * number2),
+  div: (number1, number2) => Math.floor(number1 / number2),
+  sub: (number1, number2) => Math.floor(number1 - number2),
+};
 
 module.exports = calculator;
