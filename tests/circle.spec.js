8047693f8de54532b4bc53be74c4dc4502e0db73
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-06-23 02:40:31 -0300

    Adicionei o arquivo de tests

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,26 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(5), 'object');
+
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.entries(circle(5)).length, 3);
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
+
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2).circumference, 12.56);
+
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(parseFloat((circle(3).area).toPrecision(4)), 28.26);
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const circleCopy = circle(3)
+     /* Consultado em: https://stackoverflow.com/questions/4435170/how-to-parse-float-with-two-decimal-places-in-javascript */
+    circleCopy.area = Math.round((3.14 * 3 * 3) * 100) / 100;
+    assert.deepStrictEqual(Object.values(circleCopy), [3, 28.26, 18.84]);
   });
 });
