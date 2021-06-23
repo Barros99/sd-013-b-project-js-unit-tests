cf09db3f60fa6655bcc46ebcfdc7739bfdd92d5e
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-23 10:13:38 -0300

    Requisito circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const assert = require('assert');
+const { parse } = require('path');
 const circle = require('../src/circle');
 
 /*
@@ -39,10 +40,12 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.deepStrictEqual(circle(2), {radius: 2, area: 12.56, circumference: 12.56});
 
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-   
-
-
+    for (let index in circle) {
+      parseFloat(circle[index].toPrecision(2))
+      assert.strictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
+    }    
   });
 });
