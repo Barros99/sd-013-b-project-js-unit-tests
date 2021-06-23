27fd0253d56d4fa800b6c73800ddde537ec4a737
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-22 22:52:57 -0300

    Resoluçao vqv

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,14 @@
 */
 
 const vqv = (nome, idade) => {
-  let frase = `Oi, meu nome é ${nome}! Tenho ${idade} anos,`;
+  if (!nome && !idade) {
+    return undefined;
+  }
+  let frase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return frase;
 };
 
 module.exports = vqv;
