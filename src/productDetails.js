f5ce106fc61c354fe81ea1fa2ada117d79e0dfd6
Barros99 <jeanmeneses@hotmail.com>
2021-06-23 14:15:09 -0300

    requisito seis finalizado

--- src/productDetails.js
+++ src/productDetails.js
@@ -1,3 +1,7 @@
+const assert = require('assert');
+
+const paramOne = 'Alcool gel';
+const paramTwo = 'Máscara';
 /*
   Dadas duas strings que representam nomes de produtos,
   retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -24,6 +28,8 @@
   ]
 */
 
+// Teste que os dois productIds terminam com 123.
+
 const productDetails = (firstProduct, secondProduct) => [
   {
     name: firstProduct,
@@ -38,5 +44,11 @@ const productDetails = (firstProduct, secondProduct) => [
     },
   },
 ];
-
 module.exports = productDetails;
+
+assert.strictEqual(Array.isArray(productDetails(paramOne, paramTwo)), true);
+assert.strictEqual(productDetails(paramOne, paramTwo).length, 2);
+assert.deepStrictEqual(typeof Object.values(productDetails(paramOne, paramTwo)), 'object');
+assert.notDeepStrictEqual(Object.entries(productDetails(paramOne, paramTwo)), true);
+assert.strictEqual(productDetails(paramOne, paramTwo)[1].details.productId, `${paramTwo}123`);
+assert.strictEqual(productDetails(paramOne, paramTwo)[0].details.productId, `${paramOne}123`);
