2faa209872f9a3903896a3c12d6fc1638feee058
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-06-23 02:22:02 -0300

    Arrumando problemas do linter

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,13 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (string) => {
-  return {
+const createStudent = (string) => ({
     name: string,
-    feedback: () => {
-      return 'Eita pessoa boa!'
-    }
-  }
-};
+    feedback: () => 'Eita pessoa boa!',
+  });
 
 module.exports = createStudent;
