cd82e47977beb91fe086b944aaa30722fc399580
GenesisHenriques <genesishenriques7@gmail.com>
2021-06-22 22:50:32 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,34 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    let array = productDetails('Alcool gel', 'Máscara');
+    assert.strictEqual(array.length, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof array[0], 'object');
+    assert.strictEqual(typeof array[1], 'object');
+
     // Teste que os dois objetos são diferentes entre si.
+     let verificar = (n1, n2) => {
+      if (n1 === n2){
+        return false;
+      }
+      return true;
+    }
+    assert.strictEqual(verificar(array[0],array[1]), true);
+
     // Teste que os dois productIds terminam com 123.
+    let textOne = productDetails('Alcool gel', 'Máscara')[0].details.productId;
+    let textTwo = productDetails('Alcool gel', 'Máscara')[0].details.productId;
+    let resultOne = textOne.indexOf('123') > -1;
+    let resultTwo = textTwo.indexOf('123') > -1;
+
+    assert.strictEqual((resultOne && resultTwo), true);
   });
 });
