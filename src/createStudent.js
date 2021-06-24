6c3c946066fc75b888b16eeedc2a1ed7251c39d2
Letícia Castro <aicitelks@gmail.com>
2021-06-24 18:47:28 -0300

    Concluido com sucesso: Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+
+  const objeto = {
+    name: `${nome}`,
+    feedback: () => { 
+      return 'Eita pessoa boa!' 
+    }
+  }
+
+  return objeto;
+};
 
 module.exports = createStudent;
