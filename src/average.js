64937626fd2f4471eb9581a4f72167996ca0e399
gsdu-arte <gabi15duarte@hotmail.com>
2021-06-24 12:39:17 -0300

    Resoluçao requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  let soma = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+  if (array.length === 0) {
+    return undefined;
+  }
+  const media = soma / array.length;
+  return Math.round(media);
 };
+
+// console.log(average([5, 15, 3]));
+// console.log(average(['oi', 'olá']));
+// console.log(average([]));
 
 module.exports = average;
