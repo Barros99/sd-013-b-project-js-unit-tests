642180b84235bd7def8a132014aff951fd07a1f7
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-24 19:00:57 -0300

    correção [trybe]

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,6 +79,21 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const fetchMenu = () => {};
+
+const order = () => {};
+
+const pay = () => {};
+
+const restaurant = {};
+
+const createMenu = (myMenu) => [
+  {
+    fetchMenu,
+    order, 
+    consumption: [],
+    pay, 
+  },
+];
 
 module.exports = createMenu;
