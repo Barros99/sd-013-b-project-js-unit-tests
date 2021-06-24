72982582952dbe18c9f1b05aabc89f527426c27f
Thiago <thiagodesouza.dev@gmail.com>
2021-06-24 12:10:44 -0300

    implementando a funçao calculator

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,7 +37,12 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     const outPut = productDetails('Alcool gel', 'Máscara');
     assert.strictEqual(outPut.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails()[0], 'object');
+    assert.strictEqual(typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(outPut[0], outPut[1]);
     // Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(outPut[0].details.productId, `${'Alcool gel'}123`);
+    assert.deepStrictEqual(outPut[1].details.productId, `${'Máscara'}123`);
   });
 });
