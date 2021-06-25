4f04dcd568b734a8dfae0944634951546b60a422
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-06-25 01:47:07 -0300

    numbers

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -22,9 +22,9 @@ describe("2 - Implemente os casos de teste para a função `numbers`", () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     assert.strictEqual(numbers(1), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    assert.strictEqual(numbers("1"), false);
+    assert.strictEqual(numbers('1'), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    assert.strictEqual(numbers("a"), false);
+    assert.strictEqual(numbers('a'), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
     assert.strictEqual(numbers(" "), false);
   });
