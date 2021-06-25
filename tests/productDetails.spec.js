7751cd6971a874b21f04849a6a78a17c960578bc
Letícia Castro <aicitelks@gmail.com>
2021-06-24 22:32:34 -0300

    Concluido com sucesso: Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,35 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
+
+    // Teste que o retorno da função é um array. [ok]
+    assert.deepStrictEqual(typeof productDetails(), 'object');
+
+    // Teste que o array retornado pela função contém dois itens dentro. [ok]
+    assert.deepStrictEqual(productDetails().length, 2);
+
+    // Teste que os dois itens dentro do array retornado pela função são objetos. [ok]
+    assert.deepStrictEqual(typeof productDetails('produto1', 'produto2'), 'object');
+
     // Teste que os dois objetos são diferentes entre si.
+    const produtos = productDetails('a', 'b');
+    // retorna um 'array' contendo apenas o 'valor' do objeto details de propriedade: productId, do índice 0/1 do array retornado pela função
+    const valueProductIdArray1 = Object.values(produtos[0].details);
+    const valueProductIdArray2 = Object.values(produtos[1].details);
+    // converto esse retorno de array para string
+    const valueProductIdString1 = valueProductIdArray1.toString();
+    const valueProductIdString2 = valueProductIdArray2.toString();
+    // faço a comparação de uma string pela outra
+    const compare = valueProductIdString1.includes(valueProductIdString2);
+    // verifico o requisito
+    assert.deepStrictEqual(compare, false);
+
     // Teste que os dois productIds terminam com 123.
+    const endProduct1 = valueProductIdString1.slice(-3);
+    const endProduct2 = valueProductIdString2.slice(-3);
+
+    assert.deepStrictEqual(endProduct1, '123');
+    assert.deepStrictEqual(endProduct2, '123');
   });
 });
