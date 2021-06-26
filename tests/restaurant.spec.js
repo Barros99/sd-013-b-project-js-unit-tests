9031061e9ce9ee9286938c3a4d405a37689e697f
Pedro Brito <pedroab0119@gmail.com>
2021-06-26 11:42:57 -0300

    Req1 ok?

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -48,11 +48,12 @@ const createMenu = require('../src/restaurant');
 
 describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
+    const createMenuObject = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+
     // TESTE 1: Verifique que a função createMenu() é um objeto
     // e que contém a chave fetchMenu() e que esta chave é uma função
-    // ```
-    // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
-    // ```
+    assert.strictEqual(typeof (createMenuObject.fetchMenu), 'function');
+
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
