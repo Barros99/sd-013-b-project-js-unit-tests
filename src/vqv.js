a81d4e75b0bf5f4bb825ebe177f7b83a45056f28
gsdu-arte <gabi15duarte@hotmail.com>
2021-06-26 17:12:55 -0300

    Resoluçao requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  const phrase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return phrase;
+};
+console.log(vqv('Gabi', 30));
 
 module.exports = vqv;
