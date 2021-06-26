eb12a5b7bde33afcb6feb10f0e58f859801af887
LúcioBJ <lbjunq@hotmail.com>
2021-06-26 15:42:26 -0300

    Adicionado o quinto requisito e correcao no segundo

--- src/numbers.js
+++ src/numbers.js
@@ -18,9 +18,4 @@ const numbers = (myArray) => {
   return true;
 };
 
-const assert = require('assert');
-
-assert.strictEqual(numbers([2, 3, 4]), true);
-assert.strictEqual(numbers([2, 'errado', 5]), false);
-
 module.exports = numbers;
