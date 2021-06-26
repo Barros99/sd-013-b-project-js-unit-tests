e81f7ccad00273e9b3dea80556ae800a9f9960f5
Paulo Lima <paulynho_lima@hotmail.com>
2021-06-26 15:42:03 -0300

    requisito 7 resolvido

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,23 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (num, num2) => {
+    return Math.floor(num + num2);
+  },
+  mult: (num, num2) => {
+    return Math.floor(num * num2);
+  },
+  div: (num, num2) => {
+    return Math.floor(num / num2);
+  },
+  sub: (num, num2) => {
+    return Math.floor(num - num2);
+  }
+};
+console.log(calculator.add(2.5,5));
+console.log(calculator.mult(2.5,5));
+console.log(calculator.div(3,2));
+console.log(calculator.sub(2.5,5));
 
 module.exports = calculator;
