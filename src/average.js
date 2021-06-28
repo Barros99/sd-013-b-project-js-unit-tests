f3aeb9c00a136799ff0c21ea74d41667cc93cba5
Vivian <ost.vivian@gmail.com>
2021-06-28 20:47:52 -0300

    Finalizados requisitos 1 e 2

--- src/average.js
+++ src/average.js
@@ -12,8 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  let sum = 0;
+  if (typeof array[0] !== 'number') {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+  }
+  sum += array[index];
+  }
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
