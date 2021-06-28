6c96969629ec28ae49e46f36f4f7eb5125b70cb0
Lucas Felipe Ribeiro <luc.felipe42@gmail.com>
2021-06-28 20:43:10 -0300

    req 7 e 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -3,6 +3,8 @@
 /* eslint-disable no-redeclare */
 /* eslint-disable vars-on-top */
 
+const { addLooseExports } = require("acorn");
+
 /*
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
   Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
@@ -16,10 +18,10 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
