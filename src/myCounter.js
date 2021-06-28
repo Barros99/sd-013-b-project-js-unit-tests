8404c499f4019f5189df413f1c51b849c70cd2f6
larymanzo <lamosinha@hotmail.com>
2021-06-28 15:42:49 -0300

    arrumando lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,10 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
-  console.log(myArray)
   return myArray;
 };
 
