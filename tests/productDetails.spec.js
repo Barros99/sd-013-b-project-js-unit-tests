4e7f4fdbe072b4c42b2d0704abe35f8c12c6f564
Alessandro Hugen <alessandrohugen@gmail.com>
2021-06-28 18:14:51 -0300

    Pull solicitado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -41,5 +41,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que os dois objetos são diferentes entre si.
     assert.deepStrictEqual(Object.is(Object.entries(result)), false);
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails('', '')[0].details.productId, '123');
+    assert.strictEqual(productDetails('', '')[1].details.productId, '123');
   });
 });
