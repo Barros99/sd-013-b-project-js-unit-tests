8506596224029add6eb59ad9affa5f63b1f4bff4
Guilherme Viana <guilhermehviana01@gmail.com>
2021-06-28 11:56:21 -0300

    Substitui assert.ok's para assert.deepStreetEqual's para a função circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,11 +28,11 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.ok(typeof circle(20) === 'object');
+    assert.deepStrictEqual(typeof circle(20), 'object');
     // Teste se o objeto retornado tem 3 entradas.
     assert.deepStrictEqual(circle(7), {radius: 7, area: 153.86, circumference: 43.96});
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.ok(circle() === undefined);
+    assert.deepStrictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.deepStrictEqual(circle(2), {radius: 2, area: 12.56, circumference: 12.56});
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
