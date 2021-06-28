5e969f2c4ff46f1f9f7281eb679a78f7239cef71
Guilherme Viana <guilhermehviana01@gmail.com>
2021-06-28 10:10:42 -0300

    Refatora Testes para a função circle.

--- src/circle.js
+++ src/circle.js
@@ -24,7 +24,7 @@ const circle = (radius) => {
  
   return {
     radius,
-    area: Number(parseFloat(PI * radius * radius).toPrecision(5)),
+    area: Number(parseFloat((PI * radius * radius).toPrecision(5))),
     circumference: 2 * PI * radius,
   };
 };
