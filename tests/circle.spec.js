e4b2dfb5cf8f20cf774882cfac5511ecdfdd9c0c
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-06-29 05:43:02 -0300

    Adds requirement 5.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -39,6 +39,5 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.strictEqual(circle(2).area, 12.56);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), { area: 28.259999999999998, circumference: 18.84, radius: 3 });
-    
   });
 });
