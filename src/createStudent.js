b115cfbdcec6535d850b7e860500cfadd8168bcd
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-06-29 12:14:28 -0300

    pull na master

--- src/createStudent.js
+++ src/createStudent.js
@@ -2,7 +2,7 @@
   Dada uma função chamada createStudent que recebe como parâmetro um nome,
   retorne um objeto que contenha duas chaves:
     (1) name, contendo o nome passado como parâmetro;
-    (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
+    (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!'  ao ser chamada.
 
   Faça a função da chave feedback com arrow functions!
 
