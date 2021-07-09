4095fea9c1d76b86c72467c1965f08dcce172962
r0ger <rogeriomaiajr13@gmail.com>
2021-07-09 03:06:03 -0300

    RQ 3 - refactored

--- src/vqv.js
+++ src/vqv.js
@@ -17,11 +17,9 @@
 */
 
 const vqv = (nome, idade) => {
-  const name = nome;
-  const age = idade;
-  if (name === undefined || age === undefined) return undefined;
-  return `Oi, meu nome é ${name}!
-Tenho ${age} anos,
+  if (!nome && !idade) return undefined;
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
 };
