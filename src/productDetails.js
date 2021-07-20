e256cf65ea4aa22b536558ad3542c9f2f6b4856b
GuCanalle <gucanalle@gmail.com>
2021-07-20 07:17:57 -0300

    Concluido requisito 6

--- src/productDetails.js
+++ src/productDetails.js
@@ -24,6 +24,8 @@
   ]
 */
 
+const assert = require('assert')
+
 const productDetails = (firstProduct, secondProduct) => [
   {
     name: firstProduct,
@@ -38,5 +40,20 @@ const productDetails = (firstProduct, secondProduct) => [
     },
   },
 ];
+
+console.table(Object.keys(productDetails()));
+  // Teste que o retorno da função é um array.
+  assert.deepStrictEqual(Array.isArray(productDetails()), true);
+  // Teste que o array retornado pela função contém dois itens dentro.
+  assert.deepStrictEqual(productDetails().length, 2);
+  // Teste que os dois itens dentro do array retornado pela função são objetos.
+  const test = productDetails('Alcool gel', 'Máscara');
+  assert.strictEqual(typeof (test[0]), 'object');
+  assert.strictEqual(typeof (test[1]), 'object');
+  // Teste que os dois objetos são diferentes entre si.
+  assert.notDeepStrictEqual(test[0], test[1]);
+  // Teste que os dois productIds terminam com 123.
+  assert.strictEqual(test[0].details.productId.endsWith('123'), true);
+  assert.strictEqual(test[1].details.productId.endsWith('123'), true);
 
 module.exports = productDetails;
