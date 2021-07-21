6367600bc4fbeb5928c0195f60bc3fd3d02d708e
GuCanalle <gucanalle@gmail.com>
2021-07-21 08:20:02 -0300

    Concluido? requisito? 9?

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -53,7 +53,7 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
-    assert.strictEqual(typeof (createMenu().fetchMenu) , 'function');,
+    assert.strictEqual(typeof (createMenu().fetchMenu) , 'function');
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
